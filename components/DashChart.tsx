8101a0acd5d981db8b86b93a5adfb163e96b60cf
Alejandro Romero <alexrom789@gmail.com>
2024-10-03 12:43:11 -0400

    Fix Bar Chart

--- components/DashChart.tsx
+++ components/DashChart.tsx
@@ -2,7 +2,14 @@
 
 import { Status } from "@prisma/client";
 import React from "react";
-import { BarChart, Bar, ResponsiveContainer, XAxis, YAxis } from "recharts";
+import {
+  BarChart,
+  Bar,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
 import { Card, CardHeader, CardContent, CardTitle } from "./ui/card";
 
 interface dataProps {
@@ -18,28 +25,32 @@ const DashChart = ({ data }: dataProps) => {
     <Card className="col-span-4">
       <CardHeader>
         <CardTitle>Ticket Counts</CardTitle>
-        <CardContent className="pl-2">
-          <ResponsiveContainer width="100%" height={350}>
-            <BarChart data={data}>
-              <XAxis
-                dataKey="name"
-                stroke="888888"
-                fontSize={12}
-                tickLine={false}
-                axisLine={false}
-              />
-              <YAxis
-                dataKey="name"
-                stroke="888888"
-                fontSize={12}
-                tickLine={false}
-                axisLine={false}
-              />
-              <Bar dataKey="total" fill="#60A5FA" radius={[4, 4, 0, 0]} />
-            </BarChart>
-          </ResponsiveContainer>
-        </CardContent>
       </CardHeader>
+      <CardContent className="pl-2">
+        <ResponsiveContainer width="100%" height={350}>
+          <BarChart data={data}>
+            {/* XAxis for Status Names */}
+            <XAxis
+              dataKey="name"
+              stroke="#888888"
+              fontSize={12}
+              tickLine={false}
+              axisLine={false}
+            />
+            {/* YAxis for Total Ticket Counts */}
+            <YAxis
+              stroke="#888888"
+              fontSize={12}
+              tickLine={false}
+              axisLine={false}
+            />
+            {/* Tooltip for hover */}
+            <Tooltip />
+            {/* Bar for ticket counts */}
+            <Bar dataKey="total" fill="#60A5FA" radius={[4, 4, 0, 0]} />
+          </BarChart>
+        </ResponsiveContainer>
+      </CardContent>
     </Card>
   );
 };
